68eca15bc3272b31428f55ae0bf940600f831fa5
david <davidteri91@gmail.com>
2014-11-17 08:39:07 +0100

    vendor: Use our own bootanimation and script for supported devices
    Made by @cristianomatos and @alviteri

--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -11,5 +11,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
-        vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+#        vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+         vendor/cm/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 endif

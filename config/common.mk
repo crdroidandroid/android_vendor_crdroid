0f3045c313c3c53d94c17bcaa6e42434034444c0
cristianomatos <cristianobmatos@gmail.com>
2014-12-25 23:07:41 -0200

    More cleanup

--- config/common.mk
+++ config/common.mk
@@ -73,10 +73,6 @@ ifneq ($(TARGET_BUILD_VARIANT),eng)
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
-# Copy over the changelog to the device
-PRODUCT_COPY_FILES += \
-    vendor/crdroid/CHANGELOG.mkdn:system/etc/CHANGELOG-CM.txt
-
 # Backup Tool
 ifneq ($(WITH_GMS),true)
 PRODUCT_COPY_FILES += \

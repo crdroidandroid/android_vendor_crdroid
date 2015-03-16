21490e213f91a9f8ecdd21f19675593fe7ef5672
Abhisek Devkota <ciwrl@cyanogenmod.com>
2015-03-16 11:27:10 -0700

    Fix default Alarm tone
    Hassium was deleted as a duplicate of Helium. Fix this value.
    
    Change-Id: I27e60c05ebe82d5ba400d84f5466308012a102d6

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -7,7 +7,7 @@ include vendor/crdroid/config/crdroid_audio.mk
 # Default notification/alarm sounds
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
+    ro.config.alarm_alert=Helium.ogg
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \

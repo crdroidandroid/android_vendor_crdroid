f67f322cb504069f8a07681ea307ddff647e831d
Cristiano Matos <cristianobmatos@gmail.com>
2015-10-05 08:45:49 -0300

    crdroid not cm

--- config/common.mk
+++ config/common.mk
@@ -69,7 +69,7 @@ PRODUCT_COPY_FILES +=  \
 
 # Copy over added mimetype supported in libcore.net.MimeUtils
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
+    vendor/crdroid/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \

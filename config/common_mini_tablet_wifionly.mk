6c1c30fd0572a6f3d89cdbff6e9d3e9c4fa65d5c
firebird11 <hbgassel@gmail.com>
2016-07-22 11:42:29 +0200

    crdroidify
    Change-Id: I86222f960495e9c976c27c042c43186f0fab1ca2

--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -5,7 +5,3 @@ $(call inherit-product, vendor/cm/config/common_mini.mk)
 PRODUCT_PACKAGES += \
     LatinIME
 
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-    PRODUCT_COPY_FILES += \
-        vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
-endif

8105497ac0330fd2d6b21cf99ea218e690f4f39a
David <Davidteri91@gmail.com>
2017-09-07 02:21:12 +0200

    crdroid: Add changelog generator script
    [@neobuddy89: Use author name instead of committer's name.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ build/tools/changelog.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 crDroid Android Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+export Changelog=Changelog.txt
+
+if [ -f $Changelog ];
+then
+	rm -f $Changelog
+fi
+
+touch $Changelog
+
+for i in $(seq 5);
+do
+export After_Date=`date --date="$i days ago" +%m-%d-%Y`
+k=$(expr $i - 1)
+	export Until_Date=`date --date="$k days ago" +%m-%d-%Y`
+
+	# Line with after --- until was too long for a small ListView
+	echo '====================' >> $Changelog;
+	echo  "     "$Until_Date    >> $Changelog;
+	echo '====================' >> $Changelog;
+	# Cycle through every repo to find commits between 2 dates
+	repo forall -pc 'git log --pretty=format:"%h  %s [%an]" --decorate --after=$After_Date --until=$Until_Date' >> $Changelog
+	echo >> $Changelog;
+done
+
+sed -i 's/project/   */g' $Changelog
+
+cp $Changelog $OUT/system/etc/
+cp $Changelog $OUT/
+rm $Changelog

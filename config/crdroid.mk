b0559c882a0552365dae483e630df37e27778e06
Pranav Vashi <neobuddy89@gmail.com>
2017-09-17 14:21:11 +0530

    crdroid: Set build version props properly
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -8,11 +8,12 @@ CR_VERSION := 4.0
 LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)
 LINEAGE_DISPLAY_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-v$(CR_VERSION)
 
+# Do not edit. These values are parsed by OTA and Settings.
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.crdroid.build.version=$(LINEAGE_VERSION) \
+    ro.crdroid.display.version=$(LINEAGE_DISPLAY_VERSION) \
     ro.crdroid.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.crdroid.build.version=$(CR_VERSION) \
-    ro.modversion=$(LINEAGE_DISPLAY_VERSION) \
-    ro.crdroid.display.version=$(LINEAGE_DISPLAY_VERSION)
+    ro.modversion=$(CR_VERSION)
 
 # Custom packages
 PRODUCT_PACKAGES += \

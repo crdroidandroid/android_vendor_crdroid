cf01825655905624f3ea9c6cb39ba44c7b3e50c8
Pranav Vashi <neobuddy89@gmail.com>
2017-09-25 00:58:11 +0530

    crdroid: Add more props
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -25,7 +25,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
     ro.setupwizard.rotation_locked=true \
     ro.com.google.ime.theme_id=5 \
-    ro.storage_manager.enabled=1
+    ro.storage_manager.enabled=1 \
+    ro.opa.eligible_device=true \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
+    drm.service.enabled=true \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    keyguard.no_require_sim=true \
+    persist.sys.disable_rescue=true
+
+EXCLUDE_SYSTEMUI_TESTS := true
 
 # Custom packages
 PRODUCT_PACKAGES += \

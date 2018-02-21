ae945fccb21a97b6f49daeb568f6801616b5c217
Pranav Vashi <neobuddy89@gmail.com>
2018-02-21 19:56:41 +0530

    Revert "crdroid: Tag builds as BETA till we are ready"
    This reverts commit 7d2b494dd152332225cc9730fa6e53ab45550afd.

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -16,7 +16,7 @@
 # -----------------------------------------------------------------
 # Lineage OTA update package
 
-LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/$(LINEAGE_VERSION)-BETA.zip
+LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/$(LINEAGE_VERSION).zip
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)

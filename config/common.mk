14f18d543152aa0cd58238dea3d34bc3c7154f46
Bruno Martins <bgcngm@gmail.com>
2018-05-08 12:02:36 +0100

    crdroid: Stop inheriting from no longer existing closed sources
    Change-Id: Ie0010c0c216f02cc14c91dedae15eb3e87e3d908
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -233,7 +233,6 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
--include vendor/cyngn/product.mk
 
 $(call prepend-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product, vendor/lineage/config/crdroid.mk)

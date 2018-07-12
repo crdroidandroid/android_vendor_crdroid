28f845eb4a17654407cef5474278d08eeaeb0450
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-07-12 21:26:30 +0200

    extract_utils: Newline love for adb extraction
    Change-Id: Ibbf7a03ed6920d556ed7658c277f37b02d8876b8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -984,7 +984,7 @@ function extract() {
         fi
 
         if [ "$SRC" = "adb" ]; then
-            printf '  - %s .. ' "${DST_FILE}"
+            printf '  - %s .. \n' "${DST_FILE}"
         else
             printf '  - %s \n' "${DST_FILE}"
         fi

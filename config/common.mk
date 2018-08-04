199c6bf1e3da9b8699298b147f6974ceee81a933
Michael Bestas <mkbestas@lineageos.org>
2018-08-04 17:46:35 +0300

    Revert "ota: Validate any installed data's signature against our own"
    * otasigcheck doesn't work on encrypted devices and makes
      the zip installation fail since oreo.
    * The build part of this was never ported to oreo.
    
    This reverts commit aff5e54c4ef5fec7e67e830f83ee64424005d07c.
    
    Change-Id: I411f33c1db64844091c1692ef4706ae541925d4f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -48,10 +48,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/lineage-sysconfig.xml:system/etc/sysconfig/lineage-sysconfig.xml
 
-# Signature compatibility validation
-PRODUCT_COPY_FILES += \
-    vendor/lineage/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh
-
 # init.d support
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/sysinit:system/bin/sysinit

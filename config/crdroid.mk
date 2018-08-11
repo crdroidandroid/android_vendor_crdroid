bba32cc64f2bca5b5767a79589645e51232ce13d
Pranav Vashi <neobuddy89@gmail.com>
2018-08-12 01:42:42 +0530

    crDroid 3.8.9
    Change-Id: Ieea8ebdb523e76bd3dd24cc400753d5dab80297c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -3,7 +3,7 @@ PRODUCT_VERSION_MAJOR = 7
 PRODUCT_VERSION_MINOR = 1.2
 
 # Increase CR Version with each major release.
-CR_VERSION := 3.8.8
+CR_VERSION := 3.8.9
 
 LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(CM_BUILD)-v$(CR_VERSION)
 LINEAGE_DISPLAY_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-v$(CR_VERSION)

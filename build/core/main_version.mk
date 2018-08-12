fcbcbbf141e864387d5957a833eaee22136b3804
Pranav Vashi <neobuddy89@gmail.com>
2018-08-12 14:42:56 +0530

    Move CR_VERSION to common.mk
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,13 +1,3 @@
-# Product version should match Android version
-PRODUCT_VERSION_MAJOR = 9
-PRODUCT_VERSION_MINOR = 0
-
-# Increase CR Version with each major release.
-CR_VERSION := 5.0
-
-LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)
-LINEAGE_DISPLAY_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(LINEAGE_BUILD)-v$(CR_VERSION)
-
 # crDroid System Version
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.crdroid.build.version=$(LINEAGE_VERSION) \

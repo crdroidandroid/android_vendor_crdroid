528e97c85831895f366db1c08ac36f4025484471
Pranav Vashi <neobuddy89@gmail.com>
2018-10-29 11:30:40 +0530

    build: Update kernel config settings
    * Sync with LOS.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -1,6 +1,8 @@
 # Add variables that we wish to make available to soong here.
 EXPORT_TO_SOONG := \
-    TARGET_KERNEL_ARCH \
+    KERNEL_ARCH \
+    KERNEL_CROSS_COMPILE \
+    KERNEL_MAKE_FLAGS \
     TARGET_KERNEL_CONFIG \
     TARGET_KERNEL_SOURCE
 
@@ -14,7 +16,7 @@ SOONG_CONFIG_lineageVarsPlugin :=
 
 define addVar
   SOONG_CONFIG_lineageVarsPlugin += $(1)
-  SOONG_CONFIG_lineageVarsPlugin_$(1) := $$($1)
+  SOONG_CONFIG_lineageVarsPlugin_$(1) := $$(subst ",\",$$($1))
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))

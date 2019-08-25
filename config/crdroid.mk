74b6757b45d474039fa80bf211e8fe38af0489bf
Pranav Vashi <neobuddy89@gmail.com>
2019-08-26 01:38:00 +0530

    crdroid: Disable dalvik.vm.debug.alloc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -1,5 +1,6 @@
 # Additional props
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    dalvik.vm.debug.alloc=0 \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.error.receiver.system.apps=com.google.android.gms \

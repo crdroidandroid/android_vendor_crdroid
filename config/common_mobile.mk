d9ff3c507ca2687f3d09c06994b79a5df2037988
Pranav Vashi <neobuddy89@gmail.com>
2019-09-15 23:52:51 +0530

    build: Ship as crDroid based on LOS
    * While LOS is base ROM, there are/will be additions over LOS.
      Therefore, rebrand and ship it as crDroid ROM.
    
    Change-Id: I53c407da455885af7029853ff635090f34b01f0c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -47,13 +47,6 @@ endif
 
 # Customizations
 PRODUCT_PACKAGES += \
-    IconShapePebbleOverlay \
-    IconShapeRoundedRectOverlay \
-    IconShapeSquareOverlay \
-    IconShapeSquircleOverlay \
-    IconShapeTaperedRectOverlay \
-    IconShapeTeardropOverlay \
-    IconShapeVesselOverlay \
     LineageNavigationBarNoHint \
     NavigationBarMode2ButtonOverlay
 

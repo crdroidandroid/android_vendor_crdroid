5d744bfce1db040aada4be4055b2b3cf46669d62
Pranav Vashi <neobuddy89@gmail.com>
2019-09-15 23:52:51 +0530

    build: Ship as crDroid based on LOS
    * While LOS is base ROM, there are/will be additions over LOS.
      Therefore, rebrand and ship it as crDroid ROM.
    
    Change-Id: I53c407da455885af7029853ff635090f34b01f0c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ config/crdroid.mk
@@ -0,0 +1,29 @@
+# Additional props
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    dalvik.vm.debug.alloc=0 \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dataroaming=false \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    ro.setupwizard.rotation_locked=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.opa.eligible_device=true \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1 \
+    ro.setupwizard.mode=OPTIONAL \
+    setupwizard.feature.predeferred_enabled=false \
+    drm.service.enabled=true \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    keyguard.no_require_sim=true \
+    persist.sys.disable_rescue=true
+
+# Blur
+ifneq ($(TARGET_DISABLE_BLUR), true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+endif

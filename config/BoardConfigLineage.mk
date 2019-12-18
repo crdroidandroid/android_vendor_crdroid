9b98f73fb989b850578137bd485c9b2190725fc5
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-18 18:36:05 +0000

    crdroid: Allow providing camera server and service
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/BoardConfigLineage.mk
+++ config/BoardConfigLineage.mk
@@ -10,3 +10,9 @@ include vendor/lineage/config/BoardConfigQcom.mk
 endif
 
 include vendor/lineage/config/BoardConfigSoong.mk
+
+ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/av/camera/cameraserver \
+    frameworks/av/services/camera/libcameraservice
+endif

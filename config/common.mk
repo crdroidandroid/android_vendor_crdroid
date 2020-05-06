5c7b178c7e87006b4d568be88ee9970b518f4415
Pranav Vashi <neobuddy89@gmail.com>
2020-05-07 01:11:42 +0530

    crdroid: Include faceunlock config if available
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -2,6 +2,7 @@
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/crdroid.mk)
 $(call inherit-product-if-exists, vendor/addons/config.mk)
+$(call inherit-product-if-exists, external/moto/faceunlock/config.mk)
 
 PRODUCT_BRAND ?= crDroidAndroid
 

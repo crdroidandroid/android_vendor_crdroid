255ec44577681fbaa54c815d684edb745645d488
dianlujitao <dianlujitao@lineageos.org>
2020-05-25 18:20:26 +0800

    Sort soong variables alphabetically
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -35,23 +35,23 @@ type Product_variables struct {
 	Supports_hw_fde_perf struct {
 		Cflags []string
 	}
+	Target_camera_needs_client_info struct {
+		Cflags []string
+	}
+	Target_ignores_ftp_pptp_conntrack_failure struct {
+		Cppflags []string
+	}
 	Target_init_vendor_lib struct {
 		Whole_static_libs []string
+	}
+	Target_needs_netd_direct_connect_rule struct {
+		Cppflags []string
 	}
 	Target_process_sdk_version_override struct {
 		Cppflags []string
 	}
 	Target_shim_libs struct {
 		Cppflags []string
-	}
-	Target_needs_netd_direct_connect_rule struct {
-		Cppflags []string
-	}
-	Target_ignores_ftp_pptp_conntrack_failure struct {
-		Cppflags []string
-	}
-	Target_camera_needs_client_info struct {
-		Cflags []string
 	}
 	Uses_generic_camera_parameter_library struct {
 		Srcs []string
@@ -61,10 +61,6 @@ type Product_variables struct {
 	}
 	Uses_qcom_bsp_legacy struct {
 		Cppflags []string
-	}
-	Uses_qti_camera_device struct {
-		Cppflags []string
-		Shared_libs []string
 	}
 	Uses_qcom_um_family struct {
 		Cflags []string
@@ -90,6 +86,10 @@ type Product_variables struct {
 		Header_libs []string
 		Shared_libs []string
 	}
+	Uses_qti_camera_device struct {
+		Cppflags []string
+		Shared_libs []string
+	}
 }
 
 type ProductVariables struct {
@@ -97,26 +97,26 @@ type ProductVariables struct {
 	Apply_msm8974_1440p_egl_workaround  *bool `json:",omitempty"`
 	Bootloader_message_offset  *int `json:",omitempty"`
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Qcom_um_soong_namespace  *string `json:",omitempty"`
+	Recovery_skip_ev_rel_input  *bool `json:",omitempty"`
 	Should_wait_for_qsee  *bool `json:",omitempty"`
 	Specific_camera_parameter_library  *string `json:",omitempty"`
 	Supports_extended_compress_format  *bool `json:",omitempty"`
-	Qcom_um_soong_namespace  *string `json:",omitempty"`
-	Recovery_skip_ev_rel_input  *bool `json:",omitempty"`
 	Supports_hw_fde  *bool `json:",omitempty"`
 	Supports_hw_fde_perf  *bool `json:",omitempty"`
+	Target_camera_needs_client_info  *bool `json:",omitempty"`
+	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
 	Target_init_vendor_lib  *string `json:",omitempty"`
+	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
 	Target_process_sdk_version_override  *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
-	Target_needs_netd_direct_connect_rule  *bool `json:",omitempty"`
-	Target_ignores_ftp_pptp_conntrack_failure  *bool `json:",omitempty"`
-	Target_camera_needs_client_info  *bool `json:",omitempty"`
 	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
 	Uses_nvidia_enhancements  *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy  *bool `json:",omitempty"`
-	Uses_qti_camera_device  *bool `json:",omitempty"`
 	Uses_qcom_um_family  *bool `json:",omitempty"`
 	Uses_qcom_um_3_18_family  *bool `json:",omitempty"`
 	Uses_qcom_um_4_4_family  *bool `json:",omitempty"`
 	Uses_qcom_um_4_9_family  *bool `json:",omitempty"`
 	Uses_qcom_um_4_14_family  *bool `json:",omitempty"`
+	Uses_qti_camera_device  *bool `json:",omitempty"`
 }

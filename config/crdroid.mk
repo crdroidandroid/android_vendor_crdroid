39ab50611ce61220c05af5a614fdd86bef075113
Pranav Vashi <neobuddy89@gmail.com>
2020-07-03 01:55:13 +0530

    PebbleBerry: Switch styles [3/4]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -125,3 +125,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SystemAmoledBlack \
     SystemCharcoalBlack
+
+# Switch Themes
+PRODUCT_PACKAGES += \
+    ContainedSwitch \
+    MD2Switch \
+    NarrowSwitch \
+    OnePlusSwitch \
+    RetroSwitch \
+    StockSwitch \
+    TelegramSwitch

caeb656faece2e0e59a4210aa84c819e1ded8527
Odyssey346 <57069715+Oxygemo@users.noreply.github.com>
2020-07-05 18:19:29 +0200

    Update changelog.sh

--- build/tools/changelog.sh
+++ build/tools/changelog.sh
@@ -30,7 +30,7 @@ if [ -z $changelog_days ];then
 	changelog_days=10
 else
 	if (($changelog_days > 30 )); then
-        echo "Changelog can not generated for more then 30 days. For how many days do you want to generate changelog again? (🕑 timeout 15 seconds - default to 10 days)"
+        echo "Changelog can not generated for more than 30 days. For how many days do you want to generate changelog again? (🕑 timeout 15 seconds - default to 10 days)"
         read -r -t 15 changelog_days || changelog_days=10
 	fi
 fi

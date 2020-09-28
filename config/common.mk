e2f46751ee45fd523b1abc9278acfa9e4ba1f250
Pranav Vashi <neobuddy89@gmail.com>
2020-09-28 14:22:57 +0530

    Revert "adb insecure by default"
    * Userdebug builds should be default builds as they offer more
      sepolicy relaxations compared to user builds.
    * Stop bleeding of adb insecure with this revert.
    
    This reverts commit a6c4cc93af767e6c5ac0bca195b1dc3e7da798b7.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -16,8 +16,13 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
+else
+# Enable ADB authentication
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \

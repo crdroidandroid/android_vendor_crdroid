7e75c04ccdc1c3736b2304a26ba898776cafc4c6
Pranav Vashi <neobuddy89@gmail.com>
2021-05-16 13:22:08 +0530

    build: Enable Global ThinLTO and ThinLTO cache
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -942,3 +942,8 @@ function fixup_common_out_dir() {
         mkdir -p ${common_out_dir}
     fi
 }
+
+# Enable ThinLTO Source wide.
+echo "Building with ThinLTO."
+export GLOBAL_THINLTO=true
+export USE_THINLTO_CACHE=true

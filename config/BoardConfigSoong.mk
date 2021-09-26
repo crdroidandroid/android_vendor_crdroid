8e4551487db648828ab652fe764a03e274f57778
Pranav Vashi <neobuddy89@gmail.com>
2021-09-27 02:08:42 +0530

    soong: Add TARGET_DISPLAY_USE_SMOOTH_MOTION flag
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,6 +32,7 @@ SOONG_CONFIG_lineageGlobalVars += \
     bootloader_message_offset \
     camera_needs_client_info \
     disable_postrender_cleanup \
+    display_use_smooth_motion \
     has_legacy_camera_hal1 \
     has_memfd_backport \
     ignores_ftp_pptp_conntrack_failure \
@@ -70,6 +71,7 @@ endif
 # Soong bool variables
 SOONG_CONFIG_lineageGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 SOONG_CONFIG_lineageGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
+SOONG_CONFIG_lineageGlobalVars_display_use_smooth_motion := $(TARGET_DISPLAY_USE_SMOOTH_MOTION)
 SOONG_CONFIG_lineageGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_lineageGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_lineageGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)

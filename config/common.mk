8f01a74f7e5d8b80f9c24993795ad733a2e2a486
Pranav Vashi <neobuddy89@gmail.com>
2022-03-16 23:51:04 +0530

    crdroid: Remove TrebuchetOverlay
    Change-Id: Id9e70c059d0369710c669e6a03d0feefbc3929a9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -205,8 +205,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
     DocumentsUIOverlay \
-    NetworkStackOverlay \
-    TrebuchetOverlay
+    NetworkStackOverlay
 
 # Translations
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay

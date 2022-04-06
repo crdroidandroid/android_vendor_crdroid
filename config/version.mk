d3e7d55a3bdb378c6ab5541746613c3885cd2fc1
Pranav Vashi <neobuddy89@gmail.com>
2022-04-07 01:58:37 +0530

    crdroid: Bump to version 8.4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ PRODUCT_VERSION_MAJOR = 12
 PRODUCT_VERSION_MINOR = 1
 
 # Increase CR Version with each major release.
-CR_VERSION := 8.3
+CR_VERSION := 8.4
 
 # Internal version
 ifeq ($(WITH_GMS),true)

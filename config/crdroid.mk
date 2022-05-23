7540ed17c502216ac2cfadf9352528f9eab0ed10
Andrzej Perczak <linux@andrzejperczak.com>
2022-05-23 23:51:03 +0200

    config: Properly disable blur on app launch
    ro.launcher.blur.appLaunch is 1 by default
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -33,7 +33,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 ifeq ($(TARGET_ENABLE_BLUR), true)
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.sf.blurs_are_expensive=1 \
-    ro.surface_flinger.supports_background_blur=1 \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.launcher.blur.appLaunch=0
 endif
 

52b113d1e480e396fc5d9bf4e69c10d6af7f8895
Andrzej Perczak <linux@andrzejperczak.com>
2022-06-15 11:35:45 +0200

    config: Cleanup and update common props from Pixel
     * Remove nonexistent or obsolete props.
    
     * Import new props extracted from raven.
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -1,22 +1,28 @@
 # Additional props
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    dalvik.vm.debug.alloc=0 \
-    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
-    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.error.receiver.system.apps=com.google.android.gms \
-    ro.setupwizard.enterprise_mode=1 \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
-    ro.setupwizard.rotation_locked=true \
     ro.com.google.ime.theme_id=5 \
-    ro.opa.eligible_device=true \
-    ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.setupwizard.network_required=false \
-    ro.setupwizard.gservices_delay=-1 \
-    ro.setupwizard.mode=OPTIONAL \
-    setupwizard.feature.predeferred_enabled=false \
     drm.service.enabled=true \
-    persist.sys.dun.override=0 \
-    persist.sys.disable_rescue=true
+    media.mediadrmservice.enable=true \
+    persist.sys.disable_rescue=true \
+    ro.opa.eligible_device=true
+
+# Setupwizard
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.esim_cid_ignore=00000001 \
+    ro.setupwizard.rotation_locked=true \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.lifecycle_refactoring=true \
+    setupwizard.feature.notification_refactoring=true \
+    setupwizard.feature.portal_notification=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.show_support_link_in_deferred_setup=false \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
+    setupwizard.theme=glif_v3_light
 
 # Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
 # on supported devices with Deep Press input classifier HALs and models

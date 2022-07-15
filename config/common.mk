ff8a225856ac076c0792a1589cf230e346cb7984
LuK1337 <priv.luk@gmail.com>
2022-07-16 00:38:37 +0200

    overlay: Convert NetworkStack overlay to an RRO package
    We can't use autogenerated RRO because android:targetName has to be set
    to "NetworkStackConfig".
    
    Change-Id: I6ff8a9c0dbd9607e71de5a65393328fe249a99e8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -210,6 +210,9 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/lineage/overlay/common \
     vendor/lineage/overlay/no-rro
 
+PRODUCT_PACKAGES += \
+    NetworkStackOverlay
+
 # Translations
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay

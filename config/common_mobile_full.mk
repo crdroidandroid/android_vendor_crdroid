153712a6aed5f0b5c2db6a4b22ff1dd558fcef0c
Pranav Vashi <neobuddy89@gmail.com>
2023-10-16 22:58:04 +0530

    crdroid: Remove packages moved to addons
    Change-Id: I6af87bbb53245a407ca2566c0d708ef0da5f8b4c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -3,9 +3,6 @@ $(call inherit-product, vendor/lineage/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Include GoogleSansFlex font
-$(call inherit-product-if-exists, external/google-fonts/google-sans-flex/fonts.mk)
-
 # Apps
 PRODUCT_PACKAGES += \
     Camelot \
@@ -29,11 +26,6 @@ endif
 PRODUCT_PACKAGES += \
     unrar \
     zstd
-
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    FontGoogleSansFlexOverlay
 
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries

d735c4e89cfcde7bd45aeb13521c78a09b58780b
DarkJoker360 <simoespo159@gmail.com>
2023-11-15 17:51:53 +0000

    crdroid: Bump version to 8.19
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ PRODUCT_VERSION_MAJOR = 12
 PRODUCT_VERSION_MINOR = 1
 
 # Increase CR Version with each major release.
-CR_VERSION := 8.18
+CR_VERSION := 8.19
 
 # Internal version
 ifeq ($(WITH_GMS),true)

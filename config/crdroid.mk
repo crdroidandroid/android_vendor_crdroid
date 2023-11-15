9684448a9cd00fa9e4fdbe299e7371d33a599ee5
ponces <ponces26@gmail.com>
2023-11-15 16:02:38 +0000

    crdroid: Switch to AOSPA's face unlock implementation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -64,3 +64,14 @@ PRODUCT_PACKAGES += \
     MatLog \
     OmniJaws \
     OmniStyle
+
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif

157e2c78e1617eb8957fa4e1a35375328559fc72
Pranav Vashi <neobuddy89@gmail.com>
2023-11-25 19:53:50 +0530

    common: Allow OTA downgrade regardless build type
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -47,10 +47,8 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/backuptool_ab.functions \
     system/bin/backuptool_postinstall.sh
 
-ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.ota.allow_downgrade=true
-endif
 endif
 
 # Lineage-specific broadcast actions whitelist

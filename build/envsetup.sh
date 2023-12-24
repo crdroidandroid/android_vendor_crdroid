e955a34758253cc5f8c7fb2f91ea905e5295e7d3
Pranav Vashi <neobuddy89@gmail.com>
2023-12-24 23:09:16 +0530

    crdroid: Enable Global ThinLTO flag
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -960,6 +960,10 @@ function fixup_common_out_dir() {
     fi
 }
 
+echo "Building with ThinLTO."
+export GLOBAL_THINLTO=true
+export USE_THINLTO_CACHE=true
+
 export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_DISABLE_BAZEL=true
 

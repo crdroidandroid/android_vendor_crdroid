87f03bdeafbf22dc35130c61ae2ff97f2041b90f
Alexander Grund <flamefire89@gmail.com>
2023-12-31 12:02:09 +0100

    Fix quoting in argument forward of `repopick`
    `repopick --query 'topic:Foo project:Bar'` currently fails as the
    argument to `--query` gets split making it appear as a query and change
    number argument to `repopick.py`
    So forward the correct quoting.
    
    Change-Id: I020d38216303051cbeb48cfac1c9b2f8738e02f9

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -898,7 +898,7 @@ alias cmkap='dopush cmka'
 
 function repopick() {
     T=$(gettop)
-    $T/lineage/scripts/repopick/repopick.py $@
+    $T/lineage/scripts/repopick/repopick.py "$@"
 }
 
 function sort-blobs-list() {

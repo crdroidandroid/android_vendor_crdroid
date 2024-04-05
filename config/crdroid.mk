35ad647a7402d2c3f44e09b528654de7ec12ce15
John Galt <johngaltfirstrun@gmail.com>
2024-04-05 11:22:29 -0400

    crdroid: Disable phantom process monitoring as workaround
    To workaround an AOSP activity manager crash:
    
    04-05 12:20:23.230 E/AndroidRuntime( 1599): *** FATAL EXCEPTION IN SYSTEM PROCESS: ActivityManager
    04-05 12:20:23.230 E/AndroidRuntime( 1599): java.lang.IllegalArgumentException: Comparison method violates its general contract!
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at java.util.TimSort.mergeLo(TimSort.java:781)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at java.util.TimSort.mergeAt(TimSort.java:518)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at java.util.TimSort.mergeCollapse(TimSort.java:448)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at java.util.TimSort.sort(TimSort.java:245)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at java.util.Arrays.sort(Arrays.java:1351)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at java.util.ArrayList.sort(ArrayList.java:1738)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at java.util.Collections.sort(Collections.java:208)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at com.android.server.am.PhantomProcessList.trimPhantomProcessesIfNecessary(PhantomProcessList.java:436)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at com.android.server.am.ActivityManagerConstants$$ExternalSyntheticLambda1.run(R8$$SyntheticClass:0)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at android.os.Handler.handleCallback(Handler.java:959)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at android.os.Handler.dispatchMessage(Handler.java:100)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at android.os.Looper.loopOnce(Looper.java:232)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at android.os.Looper.loop(Looper.java:317)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at android.os.HandlerThread.run(HandlerThread.java:68)
    04-05 12:20:23.230 E/AndroidRuntime( 1599):   at com.android.server.ServiceThread.run(ServiceThread.java:46)
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -30,6 +30,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.input.video_enabled=false
 
+# Workaround AOSP AM crash
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.fflag.override.settings_enable_monitor_phantom_procs=false
+
 # Blurs
 ifeq ($(TARGET_ENABLE_BLUR), true)
 PRODUCT_SYSTEM_EXT_PROPERTIES += \

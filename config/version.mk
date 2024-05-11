6934df378956659174a99f88f2b8afd264991b90
Pranav Vashi <neobuddy89@gmail.com>
2024-05-11 22:49:01 +0530

    crdroid: Bump to version 10.5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ PRODUCT_VERSION_MAJOR = 14
 PRODUCT_VERSION_MINOR = 0
 
 # Increase CR Version with each major release.
-CR_VERSION := 10.0
+CR_VERSION := 10.5
 
 # Internal version
 LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)

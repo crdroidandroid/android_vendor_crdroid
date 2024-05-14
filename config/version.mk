1963cfd958767215a681923fec7abd2a7400f21f
Pranav Vashi <neobuddy89@gmail.com>
2024-05-14 22:36:09 +0530

    crdroid: Bump to version 9.16
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -2,7 +2,7 @@ PRODUCT_VERSION_MAJOR = 13
 PRODUCT_VERSION_MINOR = 0
 
 # Increase CR Version with each major release.
-CR_VERSION := 9.0
+CR_VERSION := 9.16
 
 # Internal version
 LINEAGE_VERSION := crDroidAndroid-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)

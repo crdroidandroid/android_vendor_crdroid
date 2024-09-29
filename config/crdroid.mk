3db6b3d4ca02cd229a7a431e3532f98db573aacf
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-09-29 13:15:23 +0530

    crdroid: Build LMO Freeform app and sidebar
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -56,6 +56,8 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     BatteryStatsViewer \
     GameSpace \
+    LMOFreeform \
+    LMOFreeformSidebar \
     MatLog \
     OmniJaws \
     OmniStyle

0bd67deaaf8a75c97f8406cd1ae253e1d84af11e
Terminator-J <terminator_j@hotbutteredmonkey.com>
2025-03-23 13:51:43 -0700

    build: Update host metadata overrides for 15 QPR2
    * Matches pattern used in Pixels in 15.0 QPR2 factory images: https://dumps.tadiphone.dev/dumps/google/bluejay/-/blob/bluejay-user-15-BP1A.250405.005-13150826-release-keys/system/system/build.prop
    
    Reference:
    https://grapheneos.social/@GrapheneOS/113870255397154703
    https://github.com/GrapheneOS/platform_build/blob/15-qpr2/envsetup.sh

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -925,5 +925,5 @@ function fixup_common_out_dir() {
 export USE_THINLTO_CACHE=true
 
 # Override host metadata to make builds more reproducible and avoid leaking info
-export BUILD_USERNAME=nobody
-export BUILD_HOSTNAME=android-build
+export BUILD_USERNAME=android-build
+export BUILD_HOSTNAME=r-0123456789abcdef-acab

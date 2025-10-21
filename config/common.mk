e2011076807c4eb7b774a43ee54c8959a5d1e038
Pranav Vashi <neobuddy89@gmail.com>
2025-10-21 22:09:06 +0530

    build: Ship adb_root only WITH_SU
    * TBH - hardly any user uses adb root.
    * adb root poses root detection risks.
    * Most devices are user or userbuild type.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -233,11 +233,11 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 endif
 
 ifneq ($(TARGET_BUILD_VARIANT),user)
+ifeq ($(WITH_SU),true)
 # Root
 PRODUCT_PACKAGES += \
     adb_root
 
-ifeq ($(WITH_SU),true)
 PRODUCT_PACKAGES += \
     su
 

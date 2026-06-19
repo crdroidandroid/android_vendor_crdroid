5041304888309d7aa2cad10ff3cf904cf3eee7b4
Pranav Vashi <neobuddy89@gmail.com>
2026-06-20 02:11:08 +0530

    crdroid: Drop memtag props
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -37,13 +37,6 @@ PRODUCT_PACKAGES += \
     ColumbusService
 endif
 
-# Disable async MTE on a few processes
-PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    persist.arm64.memtag.app.com.android.se=off \
-    persist.arm64.memtag.app.com.google.android.bluetooth=off \
-    persist.arm64.memtag.app.com.android.nfc=off \
-    persist.arm64.memtag.process.system_server=off
-
 # Enable Material Design 3 Expressive
 PRODUCT_PRODUCT_PROPERTIES += is_expressive_design_enabled=true
 
